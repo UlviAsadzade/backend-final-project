04e7a8c4965347cacee800513aab4646a2c9c2fd
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-10-08 18:32:22 +1300

    Created About Page

---
+++ QuarterTemplate/QuarterTemplate/Controllers/AboutController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuarterTemplate.Models;
+using QuarterTemplate.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterTemplate.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            AboutViewModel aboutVM = new AboutViewModel
+            {
+                Abouts = _context.Abouts.ToList(),
+                Settings=_context.Settings.FirstOrDefault()
+            };
+
+            return View(aboutVM);
+        }
+    }
+}
---
+++ QuarterTemplate/QuarterTemplate/ViewModels/AboutViewModel.cs
@@ -0,0 +1,14 @@
+﻿using QuarterTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterTemplate.ViewModels
+{
+    public class AboutViewModel
+    {
+        public List<About> Abouts { get; set; }
+        public Setting Settings { get; set; }
+    }
+}
