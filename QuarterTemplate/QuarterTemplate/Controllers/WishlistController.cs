6fe0ec6f57c9347869b8cec19ff97e307e50fa03
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-10-11 00:22:32 +1300

    Created Wishlist Page

---
+++ QuarterTemplate/QuarterTemplate/Controllers/WishlistController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuarterTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterTemplate.Controllers
+{
+    public class WishlistController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WishlistController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.Include(x => x.ProductImages).ToList();
+
+            return View(products);
+        }
+    }
+}
