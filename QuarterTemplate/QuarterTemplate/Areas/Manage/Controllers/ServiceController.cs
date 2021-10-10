18db13e73271b8bfb4dd381ef4e507660afd112e
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-10-11 12:09:21 +1300

    Added CRUD for Service

---
+++ QuarterTemplate/QuarterTemplate/Areas/Manage/Controllers/ServiceController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuarterTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterTemplate.Areas.Manage.Controllers
+{
+    [Area("manage")]
+
+    public class ServiceController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Service> services = _context.Services.ToList();
+
+            return View(services);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Service service)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Services.Add(service);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Service service)
+        {
+            Service existService = _context.Services.FirstOrDefault(x => x.Id == service.Id);
+
+            if (existService == null) return NotFound();
+
+            existService.Title = service.Title;
+            existService.Icon = service.Icon;
+            existService.Order = service.Order;
+            existService.Desc = service.Desc;
+            existService.RedirectUrl = service.RedirectUrl;
+            existService.UrlText = service.UrlText;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult DeleteFetch(int id)
+        {
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null) return Json(new { status = 404 });
+
+            try
+            {
+                _context.Services.Remove(service);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(new { status = 500 });
+            }
+
+            return Json(new { status = 200 });
+        }
+    }
+}
