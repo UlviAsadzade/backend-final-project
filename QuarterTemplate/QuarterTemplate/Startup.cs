dc51e481611dfb9c3c9f51f195f1df0627527994
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-10-10 22:17:20 +1300

    Created Manage Dashboard

--- QuarterTemplate/QuarterTemplate/Startup.cs
+++ QuarterTemplate/QuarterTemplate/Startup.cs
@@ -63,6 +63,14 @@ namespace QuarterTemplate
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
